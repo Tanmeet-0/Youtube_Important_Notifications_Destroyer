3989fbeeada2452a5bdfa80e1c9c3bfcb1d6b73f
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2025-05-15 22:51:46 +0530

    added a build script to node
    typescript compiler now generates a more recent version of javascript
    
    webpack now outputs more readable javascript bundles without comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
+const Terser_Plugin = require("terser-webpack-plugin");
+const Path = require("path");
+
 module.exports = {
     entry: {
         restructure_notifications: "./src/restructure_notifications.ts",
         settings: "./src/settings.ts",
     },
-    output: { filename: "[name].js", path: __dirname + "/scripts" },
+    output: { filename: "[name].js", path: Path.resolve(__dirname, "scripts") },
     module: {
         rules: [{ test: /\.ts$/, use: "ts-loader" }],
     },
     resolve: { extensions: [".ts"] },
     mode: "none",
+    optimization: {
+        concatenateModules: true,
+        minimize: true,
+        minimizer: [
+            new Terser_Plugin({
+                terserOptions: {
+                    ecma: 2022,
+                    compress: false,
+                    mangle: false,
+                    output: { comments: false, beautify: true },
+                },
+            }),
+        ],
+    },
 };
